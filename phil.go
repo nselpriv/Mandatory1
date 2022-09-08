294a2773db1154bc4d0f6f1af35d31ec8ebeefcd
nsel <nsel@itu.dk>
2022-09-08 17:55:16 +0200

    implementation2

--- phil.go
+++ phil.go
@@ -8,7 +8,8 @@ import (
 
 type fork struct {
 	id   int
-	free chan bool
+	free  chan bool
+	status bool
 }
 type guy struct {
 	id                  int
@@ -18,6 +19,11 @@ type guy struct {
 func (g guy) eat() {
 	number := g.id
 
+	if(checkFork(g.forkright) && checkFork(g.forkleft)){
+		fmt.Println("its working")
+	} else {fmt.Println("its not working")}
+
+	//checkFork(chright, chleft, g.forkright,g.forkleft)
 
 	//if checkbooking(g.forkleft.free) {
 		if(1==1){
@@ -29,47 +35,36 @@ func (g guy) eat() {
 		time.Sleep(time.Second)
 		return
 	}
-	fmt.Println(fmt.Sprint("guy number ", number, " is done eating"))
+	//fmt.Println(fmt.Sprint("guy number ", number, " is done eating"))
+}
+
+
+func  checkFork(forky *fork) bool{
+	forky.free <- forky.status
+	availabiliy := <- forky.free
+	return availabiliy
 }
 
 func main() {
-	//ch1 := make(chan int, 2)
-
-	makeStructures()
+	begin()
 	time.Sleep(time.Second)
 
 }
-func checkstatus(c chan bool){
-	booking := <-c
-	fmt.Println(booking)
-}
 
-func checkbooking(c chan bool) bool {
-	booking := <-c
-	return booking
-}
-
-func makeStructures() {
+func begin() {
 	//making 5 forks with unique ID's and a boolean if they're taking or not
 	forks := make([]*fork, 5)
 	for i := 0; i < 5; i++ {
 		forks[i] = &fork{
-			id: i, free: make(chan bool),
-		}
-		//c := forks[1].free
-
-		//c <- true
-		//forks[1].free <- true
-		/*
-		for _, v := range forks {
-			v.free <- true
-		}
-		*/
+			id: i, free: make(chan bool,1), status: true,
+		} }
+		
 		//making 5 guys with with unique ID's and with their left and right forks
 		guys := make([]*guy, 5)
 		for i := 0; i < 5; i++ {
 			guys[i] = &guy{
 				id: i, forkleft: forks[i], forkright: forks[(i+1)%5]}
+				
 		}
 		//makes sure every guy eats 3 times
 
@@ -77,5 +72,3 @@ func makeStructures() {
 			go v.eat()
 		}
 	}
-	//go checkstatus(forks[1].free)
-}
